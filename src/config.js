19babf702de1a21c51ac51481400befbbdf7333a
loganromero <logan.m.romero@gmail.com>
2020-03-29 12:04:34 -0500

    Adding most of the front-end with React Routing configured

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "YOUR_API_GATEWAY_REGION",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "US_EAST_1",
+      USER_POOL_ID: "us-east-1_kNtQO3mW8",
+      APP_CLIENT_ID: "61l8f19i259l7583hkluvv9cj",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };
